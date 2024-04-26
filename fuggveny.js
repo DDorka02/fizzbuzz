8156a2d3bbbd2103aa76db6477bdf55f502cce64
DDorka02 <dobszaydorka@gmail.com>
2024-04-26 13:24:02 +0200

    unit tesztek

--- fuggveny.js
+++ fuggveny.js
@@ -1,23 +1,36 @@
-function fizzBuzz(szam){
-    if (szam === undefined) {
-        return "Nincs paraméter";
-    }
-    if (arguments.length > 1) {
-        return "Egynél több paraméter";
-    }
-    if (isNaN(szam)) {
-        return "Nem szám a bemenet";
-    }
+function fizzBuzz(szam) {
+  if (szam === undefined) {
+    return "Nincs paraméter";
+  }
+  if (arguments.length > 1) {
+    return "Egynél több paraméter";
+  }
+  if (isNaN(szam)) {
+    return "Nem szám a bemenet";
+  }
 
-    szam = parseInt(szam)
-    if (szam % 3 === 0) {
-        if (szam % 5 === 0) {
-            return "fizzBuzz";
-        }
-        return "fizz";
-    }else if (szam % 5 === 0) {
-        return "Buzz";
-    }else {
-        return szam;
+  szam = parseInt(szam);
+  if (szam <= 0){
+    return "Pozitív szám lehet csak a bemenet!"
+  }
+  if (szam % 3 === 0) {
+    if (szam % 5 === 0) {
+      return "fizzBuzz";
     }
+    return "fizz";
+  } else if (szam % 5 === 0) {
+    return "Buzz";
+  } else {
+    return szam;
+  }
+}
+
+function sorfizzBuzz(n) {
+  const LISTA = [];
+  let index = 1;
+  while (index <= n) {
+    LISTA.push(fizzBuzz(index));
+    index++;
+  }
+  return LISTA.join(", ");
 }
--- teszt/fuggvenyTeszt.js
+++ teszt/fuggvenyTeszt.js
@@ -1,44 +1,67 @@
-QUnit.module('fizzBuzz', function() {
-    //alap kérdések, pl. létezik-E?
-    QUnit.test('Létezik-E?', assert => {
-      assert.ok(fizzBuzz);
-    });
-
-    QUnit.test('Függvény-E?', assert => {
-        assert.ok(typeof(fizzBuzz) === 'function');
-    });
-    QUnit.test('Szám: 15', assert => {
-        assert.equal(fizzBuzz(15), "fizzBuzz");
-    });
-    QUnit.test('Szám: 5', assert => {
-        assert.equal(fizzBuzz(5), "Buzz");
-    });
-    QUnit.test('Szám: 3', assert => {
-        assert.equal(fizzBuzz(3), "fizz");
-    });
-    QUnit.test('Szám: 13', assert => {
-        assert.equal(fizzBuzz(13), 13);
-    });
-    QUnit.test('Szám szövegként: 13', assert => {
-        assert.equal(fizzBuzz("13"), 13);
-    });
-    QUnit.test('Szám szövegként: 45', assert => {
-        assert.equal(fizzBuzz("45"), "fizzBuzz");
-    });
-    QUnit.test('Szám szövegként: 6', assert => {
-        assert.equal(fizzBuzz("6"), "fizz");
-    });
-    QUnit.test('Szöveg ami nem tartalmaz számot: kutya', assert => {
-        assert.equal(fizzBuzz("kutya"), "Nem szám a bemenet");
-    });
-    QUnit.test('Tört szám: 6.6', assert => {
-        assert.equal(fizzBuzz(6.6), "fizz");
-    });
-    QUnit.test('Nincs paraméter', assert => {
-        assert.equal(fizzBuzz(), "Nincs paraméter");
-    });
-    QUnit.test('Egynél több paraméter', assert => {
-        assert.equal(fizzBuzz(3,5), "Egynél több paraméter");
-    });
-
+QUnit.module("fizzBuzz", function () {
+  //alap kérdések, pl. létezik-E?
+  QUnit.test("Létezik-E?", (assert) => {
+    assert.ok(fizzBuzz);
+  });
+  QUnit.test("Függvény-E?", (assert) => {
+    assert.ok(typeof fizzBuzz === "function");
+  });
+  QUnit.test("Szám: 15", (assert) => {
+    assert.equal(fizzBuzz(15), "fizzBuzz");
+  });
+  QUnit.test("Szám: 5", (assert) => {
+    assert.equal(fizzBuzz(5), "Buzz");
+  });
+  QUnit.test("Szám: 3", (assert) => {
+    assert.equal(fizzBuzz(3), "fizz");
+  });
+  QUnit.test("Szám: 13", (assert) => {
+    assert.equal(fizzBuzz(13), 13);
+  });
+  QUnit.test("Szám szövegként: 13", (assert) => {
+    assert.equal(fizzBuzz("13"), 13);
+  });
+  QUnit.test("Szám szövegként: 45", (assert) => {
+    assert.equal(fizzBuzz("45"), "fizzBuzz");
+  });
+  QUnit.test("Szám szövegként: 6", (assert) => {
+    assert.equal(fizzBuzz("6"), "fizz");
+  });
+  QUnit.test("Szöveg ami nem tartalmaz számot: kutya", (assert) => {
+    assert.equal(fizzBuzz("kutya"), "Nem szám a bemenet");
+  });
+  QUnit.test("Tört szám: 6.6", (assert) => {
+    assert.equal(fizzBuzz(6.6), "fizz");
+  });
+  QUnit.test("Nincs paraméter", (assert) => {
+    assert.equal(fizzBuzz(), "Nincs paraméter");
+  });
+  QUnit.test("Egynél több paraméter", (assert) => {
+    assert.equal(fizzBuzz(3, 5), "Egynél több paraméter");
+  });
+  QUnit.test("Tömb tesztelése Buzz-re", assert => {
+    let Buzzek = [5, 10, 20];
+    Buzzek.forEach = (elem => {
+        assert.equal(fizzBuzz(elem), "Buzz");
+    });  
+  });
 });
+
+  /* QUnit.test("Tömb tesztelése fizz-re", assert => {
+    let fizzek = [3, 6, 21];
+    fizzek.forEach = (elem => {
+        assert.equal(fizzBuzz(elem), "fizz");
+    });
+    });
+    QUnit.test("Tömb tesztelése fizzBuzz-re", assert => {
+        let fizzBuzzek = [30, 90, 270];
+        fizzBuzzek.forEach = (elem => {
+            assert.equal(fizzBuzz(elem), "fizzBuzz");
+        });
+    });
+    QUnit.test("Negatív vagy 0", assert => {
+        let fizzBuzzek = [-30, 0, -270];
+        fizzBuzzek.forEach = (elem => {
+            assert.equal(fizzBuzz(elem), "Pozitív szám lehet csak a bemenet!");
+        });
+    });*/
